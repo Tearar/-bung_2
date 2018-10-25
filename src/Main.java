3dd6821dd70a2a2627ca74b90a7de7f71ed2a382
Michael Adlhoch <mps.adlhoch@gmail.com>
2018-10-25 20:57:02 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+  public static void main(String[] args) {
+
+    Aufgabe1();
+    Aufgabe2();
+    // auskommentieren, was nicht benötigt wird
+  }
+
+  private static void Aufgabe1() {
+  }
+
+  private static void Aufgabe2() {
+
+  }
+}
