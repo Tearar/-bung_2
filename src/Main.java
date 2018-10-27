153b01b950ed0add6499acd346026043fc9e739a
Michael Adlhoch <mps.adlhoch@gmail.com>
2018-10-27 17:33:49 +0200

    basically full functionality

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 import baziCrypt.*;
 
+import java.io.UnsupportedEncodingException;
+
 public class Main {
 
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
         baziCrypt();
     }
 
-    private static void baziCrypt() {
+    private static void baziCrypt() throws UnsupportedEncodingException {
         BaziCrypt.decryptBaziCryptFiles();
     }
 }
